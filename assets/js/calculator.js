be04288a07bec3cff660e0585e9ab59d28ab055e
himorryyyDev <himorryyydev@gmail.com>
2025-04-24 20:37:14 +0300

    script start

---
+++ assets/js/calculator.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const displayHistory = document.querySelectorAll('.history')
+	const displayActually = document.querySelector('.actually')
+	const clearButton = document.querySelector('.clear-btn')
+	const buttons = document.querySelectorAll('.btn-grid button')
+
+	let currentInput = ''
+	let operator = ''
+	let firstNumber = ''
+	let secondNumber = ''
+	let result = ''
+})
